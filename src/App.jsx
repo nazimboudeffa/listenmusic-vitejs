89e7dc8364309819d2e215c33032252419fc1bac
nazimboudeffa <boudeffa@gmail.com>
2023-07-10 08:53:09 +0200

    Added some components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 import axios from 'axios';
+import NavBar from './components/NavBar';
 
 function App() {
   const CLIENT_ID = import.meta.env.VITE_CLIENT_ID
