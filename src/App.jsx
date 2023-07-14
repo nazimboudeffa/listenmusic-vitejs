a5bbaa525db1a51326243a46c75130e150b3fb7f
nazimboudeffa <boudeffa@gmail.com>
2023-07-15 01:09:15 +0200

    Cleaned

--- src/App.jsx
+++ src/App.jsx
@@ -78,40 +78,6 @@ function App() {
               </a>
           </div>
 
-          <div className="hidden md:block">
-              <nav aria-label="Global">
-              <ul className="flex items-center gap-6 text-sm">
-                  <li>
-                  <a
-                      className="text-gray-500 transition hover:text-gray-500/75"
-                      href="/"
-                  >
-                      About
-                  </a>
-                  </li>
-                  <li>
-                  <a
-                      className="text-gray-500 transition hover:text-gray-500/75"
-                      href="/"
-                  >
-                      Blog
-                  </a>
-                  </li>
-
-                  <li>
-                  {token &&
-                  <a
-                      className="text-gray-500 transition hover:text-gray-500/75"
-                      href="/"
-                  >
-                      Search
-                  </a>
-                  }
-                  </li>
-              </ul>
-              </nav>
-          </div>
-
           <div className="flex items-center gap-4">
               <div className="sm:flex sm:gap-4">
               {!token ?
@@ -121,26 +87,6 @@ function App() {
               }
               </div>
 
-              <div className="block md:hidden">
-              <button
-                  className="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75"
-              >
-                  <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  >
-                  <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M4 6h16M4 12h16M4 18h16"
-                  />
-                  </svg>
-              </button>
-              </div>
           </div>
           </div>
       </div>
