9a0c118cdd398c1ced5cf9c875b6f7fce958fe91
nazimboudeffa <boudeffa@gmail.com>
2023-07-15 13:49:52 +0200

    Added hits

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { useState, useEffect } from "react";
 import NavBar from './components/NavBar';
 import Hero from './components/Hero';
 import PlayLists from './components/PlayLists';
-import Footer from './components/Footer';
 import { Routes, Route } from 'react-router-dom'
 import About from './components/About';
 import HitParade from './components/HitParade';
@@ -24,10 +23,9 @@ const App = () => {
       <NavBar />
       <Routes>
         <Route path="/" element={!token ? <Hero /> : <PlayLists />} />
-        <Route path="/hit-parade" element={<HitParade />} />
+        <Route path="/hit-parade" element={!token ? <div>Please login!</div> : <HitParade />} />
         <Route path="/about" element={<About />} />
       </Routes>
-      <Footer />
     </>
   );
 }
--- src/components/HitParade.jsx
+++ src/components/HitParade.jsx
@@ -1,10 +1,84 @@
+import { useState, useEffect } from "react";
+import axios from 'axios'
+
 const HitParade = () => {
+    const [token, setToken] = useState("")
+    const [playLists, setPlayLists] = useState([])
+    const [songs, setSongs] = useState([])
+    const [selectedItemCountry, setSelectedItemCountry] = useState("default")
+    const [selectedItemPlayList, setSelectedItemPlayList] = useState("default")
+
+    useEffect(() => {
+        let token = window.localStorage.getItem("token")
+        setToken(token)
+    }, [])
+
+    const getPlayLists = async (token, country) => {
+        const {data} = await axios.get("https://api.spotify.com/v1/browse/featured-playlists?country="+country, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        console.log(data.playlists.items)
+        return data.playlists.items
+    }
+
+    const getSongsFromPlayList = async (token, playListId) => {
+        const {data} = await axios.get("https://api.spotify.com/v1/playlists/"+playListId+"/tracks", {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        console.log(data)
+        return data.items
+    }
+
+    const handleChangeCountry = (e) => {
+        setSelectedItemCountry(e.target.value)
+        getPlayLists(token, e.target.value).then(playLists => setPlayLists(playLists))
+      }
+
+    const handleChangePlayList = (e) => {
+        setSelectedItemPlayList(e.target.value)
+        getSongsFromPlayList(token, e.target.value).then(songs => {let sortedSongs = songs.sort((a, b) => b.track.popularity - a.track.popularity); setSongs(sortedSongs)})
+    }
+
     return (
-        <div>
-            <h1 className="text-xl">Top 10</h1>
-            <p>We are working on this feature</p>
-            <p>Coming soon!</p>
-        </div>
+        <section>
+            <select className="w-full border border-gray-300 rounded px-2 py-1 mb-5"  name="country" value={selectedItemCountry} onChange={(e)=>handleChangeCountry(e)}>
+                <option value="default">Select country</option>
+                <option value="DZ">Algeria</option>
+                <option value="FR">France</option>
+            </select>
+            <select className="w-full border border-gray-300 rounded px-2 py-1 mb-5"  name="order" value={selectedItemPlayList} onChange={(e)=>handleChangePlayList(e)}>
+                <option value="default">Select playlist</option>
+                {playLists.map(playList => (
+                    <option key={playList.id} value={playList.id}>{playList.name}</option>
+                ))}
+            </select>
+            <div>
+                <table className="text-sm sm:w-7/12 w-full max-w mx-auto">
+                    <thead className="text-xs text-gray-700 uppercase border-b">
+                        <tr>
+                            <th className="px-1 py-3 w-0 font-medium">Album</th>
+                            <th className="px-1 py-3 w-0 font-medium">Name</th>
+                            <th className="px-1 py-3 w-0 font-medium">Popularity</th>
+                            <th className="px-1 py-3 w-0 font-medium">Listen</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                {songs.map(song => (
+                    <tr key={song.track.id}>
+                        <td className="px-6 py-4">{song.track.album.name}</td>
+                        <td className="px-6 py-4">{song.track.name}</td>
+                        <td className="px-6 py-4">{song.track.popularity}</td>
+                        <td className="px-6 py-4"><a href={song.track.uri}>Listen</a></td>
+                    </tr>
+                ))}
+                    </tbody>
+                </table>
+            </div>
+        </section>
     );
 }
 
