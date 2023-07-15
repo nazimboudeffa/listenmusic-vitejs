746402fc638ee835ca43a7287af34486d05b80cb
nazimboudeffa <boudeffa@gmail.com>
2023-07-15 05:43:22 +0200

    Added some dscription

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,9 @@
 const About = () => {
     return (
         <div>
-            About
+            <h1 className="text-xl">About</h1>
+            <p>You have to be connected to browse the app</p>
+            <p>Create an account on Spotify then click on login</p>
         </div>
     );
 }
