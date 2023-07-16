d7146d01ebd71600d9147e49fac51a06b15cbb78
nazimboudeffa <boudeffa@gmail.com>
2023-07-16 21:07:31 +0200

    Little fix

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -3,14 +3,19 @@ import { Link } from "react-router-dom";
 export default function MobileMenu ({open}) {
     return (
         <div className={`${open? "block" : "hidden"} flex flex-col`}>
-            <Link href="/hit-parade">
+            <Link to="/hit-parade">
                 <span className="block h-16 border-t border-gray-100 leading-[4rem] pl-3">
                 Hits
                 </span>
             </Link>
-            <Link href="/about">
+            <Link to="/about">
                 <span className="block h-16 border-t border-gray-100 leading-[4rem] pl-3">
                 About
+                </span>
+            </Link>
+            <Link to="/search">
+                <span className="block h-16 border-t border-gray-100 leading-[4rem] pl-3">
+                Search
                 </span>
             </Link>
         </div>
